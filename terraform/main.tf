27d7c3018311b74f4861069a012ad23795807c95
Khalid Muzaffar <149905898+KhalidMuzaffar269553@users.noreply.github.com>
2024-04-30 11:06:09 +0300

    Update main.tf

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,91 +1,90 @@
-terraform {
-  required_providers {
-    aws = {
-      source  = "hashicorp/aws"
-      version = "~> 5.1"
-    }
-  }
-  required_version = ">= 1.2.0"
-}
-
 provider "aws" {
-  region = "us-east-1"
+  region = "us-east-1"  
 }
 
-# VPC Configuration
-resource "aws_vpc" "my_vpc" {
-  cidr_block           = "10.0.0.0/16"
+
+resource "aws_vpc" "tictactoe_vpc" {
+  cidr_block = "10.0.0.0/16"
   enable_dns_support   = true
   enable_dns_hostnames = true
+
   tags = {
-    Name = "MyVPC"
+    Name = "TicTacToeVPC"
   }
 }
 
-resource "aws_internet_gateway" "my_igw" {
-  vpc_id = aws_vpc.my_vpc.id
+
+resource "aws_internet_gateway" "gw" {
+  vpc_id = aws_vpc.tictactoe_vpc.id
+
   tags = {
-    Name = "MyInternetGateway"
+    Name = "TicTacToeGateway"
   }
 }
 
-resource "aws_subnet" "my_subnet" {
-  vpc_id     = aws_vpc.my_vpc.id
-  cidr_block = "10.0.1.0/24"
+
+resource "aws_subnet" "tictactoe_subnet" {
+  vpc_id            = aws_vpc.tictactoe_vpc.id
+  cidr_block        = "10.0.1.0/24"
   map_public_ip_on_launch = true
-  availability_zone = "us-east-1a"
+
   tags = {
-    Name = "MySubnet"
+    Name = "TicTacToeSubnet"
   }
 }
 
-resource "aws_route_table" "my_route_table" {
-  vpc_id = aws_vpc.my_vpc.id
+
+resource "aws_route_table" "tictactoe_rt" {
+  vpc_id = aws_vpc.tictactoe_vpc.id
+
   route {
     cidr_block = "0.0.0.0/0"
-    gateway_id = aws_internet_gateway.my_igw.id
+    gateway_id = aws_internet_gateway.gw.id
+  }
+
+  tags = {
+    Name = "TicTacToeRouteTable"
   }
 }
 
-resource "aws_route_table_association" "my_rta" {
-  subnet_id      = aws_subnet.my_subnet.id
-  route_table_id = aws_route_table.my_route_table.id
+resource "aws_route_table_association" "a" {
+  subnet_id      = aws_subnet.tictactoe_subnet.id
+  route_table_id = aws_route_table.tictactoe_rt.id
 }
 
-# Security Group Configuration
-resource "aws_security_group" "my_sg" {
-  vpc_id = aws_vpc.my_vpc.id
-  name = "MySecurityGroup"
-  ingress {
-    from_port   = 22
-    to_port     = 22
-    protocol    = "tcp"
-    cidr_blocks = ["0.0.0.0/0"]
-    description = "SSH"
-  }
-  ingress {
-    from_port   = 80
-    to_port     = 80
-    protocol    = "tcp"
-    cidr_blocks = ["0.0.0.0/0"]
-    description = "HTTP"
-  }
-  ingress {
-    from_port   = 8080
-    to_port     = 8080
-    protocol    = "tcp"
-    cidr_blocks = ["0.0.0.0/0"]
-    description = "HTTP for backend"
-  }
-  egress {
-    from_port   = 0
-    to_port     = 0
-    protocol    = "-1"
-    cidr_blocks = ["0.0.0.0/0"]
-  }
+
+
+resource "aws_security_group" "allow_ssh_http" {
+name = "allow_ssh_http"
+description = "Allow SSH and HTTP inbound traffic and all outbound traffic"
+vpc_id = aws_vpc.tictactoe_vpc.id
+tags = {
+Name = "allow-ssh-http"
+}
 }
 
-# EC2 Instance Configuration
+
+resource "aws_vpc_security_group_egress_rule" "allow_all_traffic_ipv4" {
+  security_group_id = aws_security_group.allow_ssh_http.id
+  cidr_ipv4 = "0.0.0.0/0"
+  ip_protocol = "-1" # all ports
+}
+resource "aws_vpc_security_group_ingress_rule" "allow_http" {
+  security_group_id = aws_security_group.allow_ssh_http.id
+  cidr_ipv4 = "0.0.0.0/0"
+  ip_protocol = "tcp"
+  from_port = 8080
+  to_port = 8081
+}
+resource "aws_vpc_security_group_ingress_rule" "allow_ssh" {
+  security_group_id = aws_security_group.allow_ssh_http.id
+  cidr_ipv4 = "0.0.0.0/0"
+  ip_protocol = "tcp"
+  from_port = 22
+  to_port = 22
+}
+
+
 resource "aws_instance" "my_instance" {
   ami                    = "ami-080e1f13689e07408"  
   instance_type          = "t2.micro"
@@ -107,7 +106,6 @@ resource "aws_instance" "my_instance" {
   EOF
 
   tags = {
-    Name = "MyDockerInstance"
+    Name = "MyTicTacInstance"
   }
 }
-
